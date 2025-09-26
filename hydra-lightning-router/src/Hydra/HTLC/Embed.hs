b310c566ed94afc755a44fb075b72ef51ca082fc
Daniel Firth <dan.firth@homotopic.tech>
2025-09-26 09:34:01 +0000

    Decode plutus.json at compile time

--- hydra-lightning-router/src/Hydra/HTLC/Embed.hs
+++ hydra-lightning-router/src/Hydra/HTLC/Embed.hs
@@ -8,16 +8,23 @@ import Cardano.Api.Shelley (PlutusScript (PlutusScriptSerialised))
 import Control.Lens ((^.))
 import Data.Aeson qualified as Aeson
 import Data.Aeson.Lens (key, nth, _String)
+import Data.ByteString qualified as BS
 import Data.ByteString.Base16 qualified as Base16
 import Data.ByteString.Short (toShort)
-import Data.FileEmbed (embedFile, makeRelativeToProject)
+import Data.FileEmbed (makeRelativeToProject)
 import Data.Text.Encoding (encodeUtf8)
+import Language.Haskell.TH (runIO)
+import Language.Haskell.TH.Syntax (lift)
 
 plutusJSON :: Aeson.Value
 plutusJSON =
-    case Aeson.decodeStrict $(makeRelativeToProject "./plutus.json" >>= embedFile) of
-        Nothing -> error "Invalid blueprint: plutus.json"
-        Just value -> value
+    $( do
+        path <- makeRelativeToProject "./plutus.json"
+        bs <- runIO $ BS.readFile path
+        case Aeson.decodeStrict bs of
+          Nothing -> fail "Invalid plutus.json file."
+          Just (value :: Aeson.Value) -> lift value
+     )
 
 htlcValidatorScript :: PlutusScript PlutusScriptV3
 htlcValidatorScript =
