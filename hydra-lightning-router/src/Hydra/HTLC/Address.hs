7c41cec69bdfa96595d147d39f991a0c0cfc379b
Daniel Firth <daniel.firth@iohk.io>
2025-10-09 09:27:30 +0000

    Add Hydra.HTLC.Data

--- hydra-lightning-router/src/Hydra/HTLC/Address.hs
+++ hydra-lightning-router/src/Hydra/HTLC/Address.hs
@@ -1,18 +1,17 @@
 module Hydra.HTLC.Address (
-  makeHtlcScriptAddress
-  ) where
+    makeHtlcScriptAddress,
+) where
 
-import Cardano.Api (NetworkId, ShelleyBasedEra, AddressInEra, PaymentCredential(PaymentCredentialByScript), makeShelleyAddressInEra, Script(PlutusScript), hashScript, StakeAddressReference(NoStakeAddress), plutusScriptVersion)
+import Cardano.Api (AddressInEra, NetworkId, PaymentCredential (PaymentCredentialByScript), Script (PlutusScript), ShelleyBasedEra, StakeAddressReference (NoStakeAddress), hashScript, makeShelleyAddressInEra, plutusScriptVersion)
 import Hydra.HTLC.Embed (htlcValidatorScript)
 
-
-makeHtlcScriptAddress
-  :: ShelleyBasedEra era
-  -> NetworkId
-  -> AddressInEra era
+makeHtlcScriptAddress ::
+    ShelleyBasedEra era ->
+    NetworkId ->
+    AddressInEra era
 makeHtlcScriptAddress sbe networkId =
-  makeShelleyAddressInEra
-    sbe
-    networkId
-    (PaymentCredentialByScript $ hashScript $ PlutusScript plutusScriptVersion htlcValidatorScript)
-    NoStakeAddress
+    makeShelleyAddressInEra
+        sbe
+        networkId
+        (PaymentCredentialByScript $ hashScript $ PlutusScript plutusScriptVersion htlcValidatorScript)
+        NoStakeAddress
---
+++ hydra-lightning-router/src/Hydra/HTLC/Data.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Hydra.HTLC.Data (
+    Datum (Datum),
+    Redeemer (Claim, Refund),
+)
+where
+
+import Data.Kind (Type)
+import PlutusLedgerApi.V3 qualified as V3
+import PlutusTx qualified
+import PlutusTx.Prelude (BuiltinByteString)
+import Prelude qualified as Haskell
+
+type Datum :: Type
+data Datum
+    = Datum
+    { hash :: BuiltinByteString
+    , timeout :: V3.POSIXTime
+    , sender :: BuiltinByteString
+    , receiver :: BuiltinByteString
+    }
+    deriving stock (Haskell.Eq, Haskell.Show, Haskell.Ord)
+
+PlutusTx.unstableMakeIsData ''Datum
+
+type Redeemer :: Type
+data Redeemer
+    = Claim BuiltinByteString
+    | Refund
+
+PlutusTx.unstableMakeIsData ''Redeemer
