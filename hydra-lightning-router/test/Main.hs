870098d94c2d113ed3fb2ccac8bc748c48c9370e
Daniel Firth <daniel.firth@iohk.io>
2025-10-14 12:50:14 +0000

    Add mockDatum

--- hydra-lightning-router/test/Main.hs
+++ hydra-lightning-router/test/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main
   ( main,
   )
@@ -12,7 +14,9 @@ import Convex.MockChain.Defaults qualified as Defaults
 import Convex.MockChain.Utils (mockchainSucceeds)
 import Convex.Utils (failOnError, inBabbage)
 import Convex.Wallet.MockWallet qualified as Wallet
+import Hydra.HTLC.Data (Datum (Datum))
 import Hydra.HTLC.Embed (htlcValidatorScript)
+import PlutusTx (ToData)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (testCase)
 
@@ -23,16 +27,18 @@ alwaysSucceedsScript :: C.PlutusScript C.PlutusScriptV1
 alwaysSucceedsScript = C.examplePlutusScriptAlwaysSucceeds C.WitCtxTxIn
 
 payToPlutusScript ::
-  forall era lang m.
+  forall era lang m a.
   (MonadFail m) =>
   (C.MonadError (BalanceTxError era) m) =>
   (C.IsPlutusScriptLanguage lang) =>
   (MonadMockchain era m) =>
   (C.IsBabbageBasedEra era) =>
+  (ToData a) =>
   C.PlutusScript lang ->
+  a ->
   m C.TxIn
-payToPlutusScript script = inBabbage @era $ do
-  let tx = execBuildTx $ payToScriptDatumHash Defaults.networkId (plutusScript script) () C.NoStakeAddress (C.lovelaceToValue 10_000_000)
+payToPlutusScript script datum = inBabbage @era $ do
+  let tx = execBuildTx $ payToScriptDatumHash Defaults.networkId (plutusScript script) datum C.NoStakeAddress (C.lovelaceToValue 10_000_000)
   i <- C.getTxId . C.getTxBody <$> CoinSelection.tryBalanceAndSubmit mempty Wallet.w1 tx TrailingChange []
   pure (C.TxIn i (C.TxIx 0))
 
@@ -54,15 +60,18 @@ spendFromPlutusScript script ref = inBabbage @era $ do
 main :: IO ()
 main = defaultMain tests
 
+mockDatum :: Datum
+mockDatum = Datum "0000000000" 0 "foo" "bar"
+
 tests :: TestTree
 tests =
   testGroup
     "unit tests"
     [ testGroup
         "payments"
-        [ testCase "spending a public key output to alwaysSucceeds script" (mockchainSucceeds $ failOnError $ payToPlutusScript alwaysSucceedsScript),
-          testCase "spending a script output from alwaysSucceeds script" (mockchainSucceeds $ failOnError (payToPlutusScript alwaysSucceedsScript >>= spendFromPlutusScript alwaysSucceedsScript)),
-          testCase "spending a public key output to htlc script" (mockchainSucceeds $ failOnError $ payToPlutusScript htlcValidatorScript),
-          testCase "spending a script output from htlc script" (mockchainSucceeds $ failOnError (payToPlutusScript htlcValidatorScript >>= spendFromPlutusScript htlcValidatorScript))
+        [ testCase "spending a public key output to alwaysSucceeds script" (mockchainSucceeds $ failOnError $ payToPlutusScript alwaysSucceedsScript ()),
+          testCase "spending a script output from alwaysSucceeds script" (mockchainSucceeds $ failOnError (payToPlutusScript alwaysSucceedsScript () >>= spendFromPlutusScript alwaysSucceedsScript)),
+          testCase "spending a public key output to htlc script" (mockchainSucceeds $ failOnError $ payToPlutusScript htlcValidatorScript ()),
+          testCase "spending a script output from htlc script" (mockchainSucceeds $ failOnError (payToPlutusScript htlcValidatorScript mockDatum >>= spendFromPlutusScript htlcValidatorScript))
         ]
     ]
