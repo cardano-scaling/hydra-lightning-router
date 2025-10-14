09e8c4d31267d35aced3b866ece4ac29f136198d
Daniel Firth <daniel.firth@iohk.io>
2025-10-14 12:33:58 +0000

    Use sc-tools to write smoke test for htlc validator

--- hydra-lightning-router/test/Main.hs
+++ hydra-lightning-router/test/Main.hs
@@ -3,5 +3,66 @@ module Main
   )
 where
 
+import Cardano.Api qualified as C
+import Convex.BuildTx (execBuildTx, payToScriptDatumHash, spendPlutus)
+import Convex.Class (MonadMockchain)
+import Convex.CoinSelection (BalanceTxError, ChangeOutputPosition (TrailingChange))
+import Convex.MockChain.CoinSelection qualified as CoinSelection
+import Convex.MockChain.Defaults qualified as Defaults
+import Convex.MockChain.Utils (mockchainSucceeds)
+import Convex.Utils (failOnError, inBabbage)
+import Convex.Wallet.MockWallet qualified as Wallet
+import Hydra.HTLC.Embed (htlcValidatorScript)
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase)
+
+plutusScript :: (C.IsPlutusScriptLanguage lang) => C.PlutusScript lang -> C.Script lang
+plutusScript = C.PlutusScript C.plutusScriptVersion
+
+alwaysSucceedsScript :: C.PlutusScript C.PlutusScriptV1
+alwaysSucceedsScript = C.examplePlutusScriptAlwaysSucceeds C.WitCtxTxIn
+
+payToPlutusScript ::
+  forall era lang m.
+  (MonadFail m) =>
+  (C.MonadError (BalanceTxError era) m) =>
+  (C.IsPlutusScriptLanguage lang) =>
+  (MonadMockchain era m) =>
+  (C.IsBabbageBasedEra era) =>
+  C.PlutusScript lang ->
+  m C.TxIn
+payToPlutusScript script = inBabbage @era $ do
+  let tx = execBuildTx $ payToScriptDatumHash Defaults.networkId (plutusScript script) () C.NoStakeAddress (C.lovelaceToValue 10_000_000)
+  i <- C.getTxId . C.getTxBody <$> CoinSelection.tryBalanceAndSubmit mempty Wallet.w1 tx TrailingChange []
+  pure (C.TxIn i (C.TxIx 0))
+
+spendFromPlutusScript ::
+  forall era lang m.
+  (MonadFail m) =>
+  (MonadMockchain era m) =>
+  (C.MonadError (BalanceTxError era) m) =>
+  (C.HasScriptLanguageInEra lang era) =>
+  (C.IsPlutusScriptLanguage lang) =>
+  (C.IsBabbageBasedEra era) =>
+  C.PlutusScript lang ->
+  C.TxIn ->
+  m C.TxId
+spendFromPlutusScript script ref = inBabbage @era $ do
+  let tx = execBuildTx $ spendPlutus ref script () ()
+  C.getTxId . C.getTxBody <$> CoinSelection.tryBalanceAndSubmit mempty Wallet.w1 tx TrailingChange []
+
 main :: IO ()
-main = putStrLn "Test suite not implemented."
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+  testGroup
+    "unit tests"
+    [ testGroup
+        "payments"
+        [ testCase "spending a public key output to alwaysSucceeds script" (mockchainSucceeds $ failOnError $ payToPlutusScript alwaysSucceedsScript),
+          testCase "spending a script output from alwaysSucceeds script" (mockchainSucceeds $ failOnError (payToPlutusScript alwaysSucceedsScript >>= spendFromPlutusScript alwaysSucceedsScript)),
+          testCase "spending a public key output to htlc script" (mockchainSucceeds $ failOnError $ payToPlutusScript htlcValidatorScript),
+          testCase "spending a script output from htlc script" (mockchainSucceeds $ failOnError (payToPlutusScript htlcValidatorScript >>= spendFromPlutusScript htlcValidatorScript))
+        ]
+    ]
