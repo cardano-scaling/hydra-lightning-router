fe27c945fe7ed0a03721a82de114c763fbdb602b
Daniel Firth <daniel.firth@iohk.io>
2025-10-16 10:06:54 +0000

    Add horizon cache to nix flake

--- flake.nix
+++ flake.nix
@@ -24,10 +24,12 @@
     extra-substituters = [
       "https://cache.iog.io"
       "https://cardano-scaling.cachix.org"
+      "https://horizon.cachix.org"
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "cardano-scaling.cachix.org-1:QNK4nFrowZ/aIJMCBsE35m+O70fV6eewsBNdQnCSMKA="
+      "horizon.cachix.org-1:MeEEDRhRZTgv/FFGCv3479/dmJDfJ82G6kfUDxMSAw0="
     ];
     allow-import-from-derivation = true;
   };
