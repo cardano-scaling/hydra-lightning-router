4efd80861860bd513d5592081e2935d1367222e4
Noon van der Silk <noon.vandersilk@iohk.io>
2025-10-24 21:23:36 +0100

    make it easy to run process-compose

--- nix/packages.nix
+++ nix/packages.nix
@@ -5,6 +5,7 @@
         hydra-lightning-router = final.callCabal2nix "hydra-lightning-router" (lib.cleanSource "${inputs.self}/hydra-lightning-router") { };
       };
       legacyPackages = inputs.horizon.legacyPackages.${system}.extend myOverlay;
+      pkgs = import inputs.nixpkgs { inherit system; };
     in
     rec {
 
@@ -12,6 +13,7 @@
         packages = p: [ p.hydra-lightning-router ];
         buildInputs = [
           legacyPackages.cabal-install
+          pkgs.process-compose
           inputs.cardano-node.packages.${system}.cardano-node
           inputs.cardano-node.packages.${system}.cardano-cli
           inputs.hydra.packages.${system}.hydra-node
